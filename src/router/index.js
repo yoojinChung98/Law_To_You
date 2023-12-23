60d9ef1a3f2b6dfd8d41b2832ee576f04c9fec66
박지수 <parkjisu0107@gmail.com>
2023-12-23 16:16:05 +0900

    router 영역 wrapper component 추가

--- src/router/index.js
+++ src/router/index.js
@@ -9,6 +9,7 @@ import JoinMain from "../components/Join/JoinMain";
 import LoginMain from "../components/Login/LoginMain";
 import MainPage from "../components/MainPage";
 import commUtil from "../util/commUtil";
+import MainLayout from "./MainLayout";
 
 const RouterIndex = () => {
   const isLogin = commUtil.isNotEmpty(localStorage.getItem("accessToken"));
@@ -32,11 +33,13 @@ const RouterIndex = () => {
       <Route path="/join/" element={<JoinMain />} />
       <Route path="/" element={<MainPage />}></Route>
       <Route path="/*" element={<></>}></Route>
-      <Route path="/counsel/" element={<BoardCounsel />}></Route>
-      <Route path="/free/" element={<BoardFree />}></Route>
-      <Route path="/freewrite/" element={<BoardFreeWrite />}></Route>
-      <Route path="/freereply/" element={<BoardFreeReply />}></Route>
-      <Route path="/bupbong/" element={<Bupbong />}></Route>
+      <Route element={<MainLayout />}>
+        <Route path="/counsel/" element={<BoardCounsel />}></Route>
+        <Route path="/free/" element={<BoardFree />}></Route>
+        <Route path="/freewrite/" element={<BoardFreeWrite />}></Route>
+        <Route path="/freereply/" element={<BoardFreeReply />}></Route>
+        <Route path="/bupbong/" element={<Bupbong />}></Route>
+      </Route>
     </Routes>
   );
 };
